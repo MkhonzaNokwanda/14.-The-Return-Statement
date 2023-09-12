3972ba42cef736bcad4f162367aab06158e1459e
Dell-User <Dell-User@DESKTOP-B16SGH2>
2023-09-13 00:04:48 +0200

    Add project files.

---
+++ 14. The Return Statement/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.The_Return_Statement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int x =Convert.ToInt32(Console.ReadLine());
+            
+           Console.WriteLine(Cube(x));
+
+            //freeze console
+            Console.ReadLine();
+        }
+     //making of the cube method
+        static int Cube(int x)
+        {
+            Console.WriteLine("The cube of the number you entered is ");
+            return x * x * x;
+        }
+    }
+}
